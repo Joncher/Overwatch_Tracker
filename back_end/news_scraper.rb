4a4051145d103e17dcbebb8142d71308d23c6229
John Oncher <johnoncher@gmail.com>
2019-04-02 10:49:42 -0400

    changes

---
+++ back_end/news_scraper.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+require 'nokogiri'
+require 'byebug'
+def scraper
+  url = 'https://playoverwatch.com/en-us/news'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  news_videos = parsed_page.css('.u-video')
+  news_hash = news_videos.map do |news|
+    {link_url: "#{news.attributes["href"].value}",
+    image: "#{news.children[0].values[1].slice(/bn.*g/)}",
+    header:"#{news.css('h3').children.text}"
+  }
+  end
+  byebug
+end
+scraper
