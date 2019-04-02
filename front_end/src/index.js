76f461b21605140e4db2712cc116aa72d6934aae
John Oncher <johnoncher@gmail.com>
2019-04-02 13:57:11 -0400

    set up redux

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./Redux/index";
 import * as serviceWorker from "./serviceWorker";
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
